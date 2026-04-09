17305978eb0a28610f669ad1a41bb7b53fca2d24
Sakshi Kumari <sakshik170@gmail.com>
2026-04-09 22:26:47 +0530

    Time: 18 ms (78.13%) | Memory: 139.3 MB (43.55%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,14 +4,13 @@ public:
         deque<int> q;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            // if element is smaller in dequeue but next max is coming from same window 
             while(!q.empty() && nums[q.back()]<=nums[i]){
                 q.pop_back();
             }
+            q.push_back(i);
             if(!q.empty() && q.front()==i-k){
                 q.pop_front();
             }
-            q.push_back(i);
             if(i>=k-1){
                 ans.push_back(nums[q.front()]);
             }
