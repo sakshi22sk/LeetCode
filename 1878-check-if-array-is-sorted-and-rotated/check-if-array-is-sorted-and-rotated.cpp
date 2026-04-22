bbde9351bae7769e7ab20b930a569771c470a125
Sakshi Kumari <sakshik170@gmail.com>
2026-04-22 11:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (82.21%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        int k = -1;
-
-        // find break point
-        for(int i = 0; i < n-1; i++){
-            if(nums[i] > nums[i+1]){
-                k = i + 1;  // ✅ correct split point
-                break;
+        int k=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                k++;
             }
         }
-
-        // already sorted
-        if(k == -1) return true;
-
-        vector<int> temp = nums; // ✅ work on copy
-
-        // reverse logic to undo rotation
-        reverse(temp.begin(), temp.end());
-        reverse(temp.begin(), temp.begin() + (n - k));
-        reverse(temp.begin() + (n - k), temp.end());
-
-        // check sorted
-        return is_sorted(temp.begin(), temp.end());
+        if(nums[0]<nums[nums.size()-1]){
+            k++;
+        }
+         
+        return k<=1;
     }
 };
